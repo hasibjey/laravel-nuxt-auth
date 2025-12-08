d3c8f229b27ad4caf70db5f15bb32bcca0196e01
hasibjey <hasibjey@gmail.com>
2025-12-08 17:43:23 +0600

    Auto logout plugin

---
+++ frontend/app/plugins/autoLogout.client.js
@@ -0,0 +1,25 @@
+export default defineNuxtPlugin((nuxtApp) => {
+    const auth = useAuthStore()
+
+    let timer
+    const INACTIVITY_LIMIT = 30 * 60 * 1000
+
+    const resetTimer = () => {
+        clearTimeout(timer)
+        if (auth.isAuthenticated) {
+            timer = setTimeout(() => {
+                auth.logout()
+                // alert('You have been logged out due to inactivity')
+            }, INACTIVITY_LIMIT)
+        }
+    }
+
+    // Listen for activity events
+    window.addEventListener('mousemove', resetTimer)
+    window.addEventListener('keydown', resetTimer)
+    window.addEventListener('click', resetTimer)
+    window.addEventListener('scroll', resetTimer)
+    window.addEventListener('touchstart', resetTimer)
+
+    resetTimer()
+})
